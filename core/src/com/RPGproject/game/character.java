6a4d76683665c1ac0e7a0f1f0342a941d6469e0c
William <wliversidge88@gmail.com>
2020-11-20 08:53:13 +0000

    Added in base weapon file

--- core/src/com/RPGproject/game/character.java
+++ core/src/com/RPGproject/game/character.java
@@ -1,4 +1,17 @@
 package com.RPGproject.game;
 
 public class character extends entity{
+    private Weapon weapon;
+
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public character() {
+    }
 }
