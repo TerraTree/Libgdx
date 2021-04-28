4448bfd6946f2202d5af2db1016a68ad6d208e81
William Liversidge <wliversidge88@gmail.com>
2021-04-28 15:21:38 +0100

    Delete stageInterface.java

--- stageInterface.java
+++
@@ -1,9 +0,0 @@
-package com.RPGproject.game;
-
-import com.badlogic.gdx.graphics.Texture;
-
-public interface stageInterface {
-    void startGame();
-    void returnToStart();
-    Texture getTexture(Textures texture);
-}
